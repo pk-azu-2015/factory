6a63f88d00944b0a309671dc22f16d7350a42b8b
student <student>
2015-11-17 20:13:50 +0400

    add new method and a bit more

--- src/RecyclingWebApp/src/main/java/com/factory/recyclingwebapp/Recycling.java
+++ src/RecyclingWebApp/src/main/java/com/factory/recyclingwebapp/Recycling.java
@@ -5,6 +5,8 @@
  */
 package com.factory.recyclingwebapp;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -24,5 +26,31 @@ public class Recycling {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !\nHave a nice day!";
+    }
+    
+    @WebMethod(operationName = "handleWithRecycling")
+    public void handleWithRecycling() {
+        List<Integer> resources = new ArrayList<>();
+        Integer idCrapu = 5;//Zepsuty produkt
+        
+       // Magazyn.
+        
+    }
+    
+    @WebMethod(operationName = "getCrapObjectList")
+    public List<Integer> getCrapObjectList(@WebParam(name = "idStanu") Integer idStanu) {
+        List<Integer> resources = new ArrayList<>();
+        return resources ;
+    }
+    
+    @WebMethod(operationName = "makeRecycling")
+    public String makeRecycling(@WebParam(name = "idProduktu") Integer idProduktu, @WebParam(name = "idStanu") Integer idStanu) {
+        return "idStanu: " + idStanu + "\n"
+                + " idProduktu: " + idProduktu;
+    }
+    
+    @WebMethod(operationName = "addResource")
+    public void addResource(@WebParam(name = "ilosc") Integer ilosc) {
+        
     }
 }
