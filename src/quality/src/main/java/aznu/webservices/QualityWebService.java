a11aaf41690f0377ee846bd0345dd1874cec9fe9
marcin <szewczykus@gmail.com>
2015-12-15 13:08:21 +0100

    added method sprawdzStan

--- src/quality/src/main/java/aznu/webservices/QualityWebService.java
+++ src/quality/src/main/java/aznu/webservices/QualityWebService.java
@@ -5,9 +5,13 @@
  */
 package aznu.webservices;
 
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.xml.ws.WebServiceRef;
+import localhost._8080.magazyn.Magazyn;
+import localhost._8080.magazyn.Magazyn_Service;
 
 /**
  *
@@ -15,12 +19,27 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "QualityWebService")
 public class QualityWebService {
+  
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/Magazyn/Magazyn.wsdl")
+    private Magazyn_Service service;
 
     /**
      * This is a sample web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "sprawdzStan")
+    public String sprawdzStan(@WebParam(name = "name") Norma norma) {        
+        try { // Call Web Service Operation
+            localhost._8080.magazyn.Magazyn port = service.getMagazynPort();
+            java.util.List<java.lang.Integer> productIdList = port.zwrocListeIDProduktow(Stan.DO_KONTROLI.getStanId());
+            
+            if(productIdList != null){
+                for(Integer productId : productIdList ){
+                    port.wezProdukt(productId, Stan.DO_KONTROLI.getStanId());
+                }
+            }
+            return "Hello " + norma + " !" + String.join(",",productIdList.toString());
+        } catch (Exception ex) {
+            return "ERROR connecting with Magazyn Service";
+        }
     }
 }
